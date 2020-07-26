e28966a154c9516c9919fed2409624e84aadcbc9
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2020-07-26 12:21:55 +0100

    SRP applied to CsvWriter class

--- src/CsvWriter.java
+++ src/CsvWriter.java
@@ -7,21 +7,24 @@ public class CsvWriter {
     public void writeCsv(TriangleGroup triangleGroup, ProductGroup productGroup, String filename) throws IOException {
 
         try (BufferedWriter csvWriter = new BufferedWriter(new FileWriter(filename))) {
-
-            //first line of the output file is earliest origin year + range of years
-            csvWriter.append(Integer.toString(productGroup.getLowestOriginYear()));
-            csvWriter.append(",");
-            csvWriter.append(Integer.toString(productGroup.getRangeOfYears()));
-            csvWriter.append("\n");
-
-            //every other line should be a Triangle
-            for (Triangle triangle : triangleGroup.getTriangles().values()){
-                csvWriter.append(String.join(",", triangle.getTriangleData()));
-                csvWriter.append("\n");
-            }
-
+            writeFirstLine(csvWriter, productGroup);
+            writeTriangles(csvWriter, triangleGroup);
         }
 
+    }
+
+    private void writeFirstLine(BufferedWriter csvWriter, ProductGroup productGroup) throws IOException{
+        csvWriter.append(Integer.toString(productGroup.getLowestOriginYear()));
+        csvWriter.append(",");
+        csvWriter.append(Integer.toString(productGroup.getRangeOfYears()));
+        csvWriter.append("\n");
+    }
+
+    private void writeTriangles(BufferedWriter csvWriter, TriangleGroup triangleGroup) throws IOException{
+        for (Triangle triangle : triangleGroup.getTriangles().values()){
+            csvWriter.append(String.join(",", triangle.getTriangleData()));
+            csvWriter.append("\n");
+        }
     }
 
 }
